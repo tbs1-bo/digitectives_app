3d8c0a395b02623793bbd677a85038aa0cc9b549
Christoph Kaiser <kaiser@tbs1.de>
2021-11-06 00:29:47 +0100

    reintegrate ListView in HomeFragment

--- app/src/main/java/com/jankrb/fff_layout/ui/HomeFragment.kt
+++ app/src/main/java/com/jankrb/fff_layout/ui/HomeFragment.kt
@@ -7,13 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.jankrb.fff_layout.MainActivity
 import com.jankrb.fff_layout.R
 import com.jankrb.fff_layout.dbclasses.Scan
 import com.jankrb.fff_layout.dbclasses.ScanDao
 import com.jankrb.fff_layout.dbclasses.dbqueries.sendToOnlineDatabase
 import com.jankrb.fff_layout.dbclasses.dbvar
+import com.jankrb.fff_layout.home.HomeNewsListAdapter
 import com.jankrb.fff_layout.objects.PrivateSettings
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -28,10 +31,10 @@ class HomeFragment : Fragment() {
     private val createdAts: MutableList<String> = mutableListOf()
     private val titles: MutableList<String> = mutableListOf()
     private val descriptions: MutableList<String> = mutableListOf()
-    private lateinit var scanView: TextView //um auf Objekt aus anderen Methoden zugreifen zu können
     private lateinit var totalScannedView: TextView
     private lateinit var numberTypesView: TextView
     private lateinit var numberUnsyncedView: TextView
+    private lateinit var listView: ListView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -40,54 +43,30 @@ class HomeFragment : Fragment() {
         // Home Sync Btn
         val sync_btn: Button = root.findViewById(R.id.sync_data_btn)
 
-        scanView = root.findViewById(R.id.home_recent_informations)
+        //scanView = root.findViewById(R.id.home_recent_informations)
         totalScannedView = root.findViewById(R.id.stats_box_self_value)
         numberTypesView = root.findViewById(R.id.stats_box_types_value)
         numberUnsyncedView = root.findViewById(R.id.stats_box_unsynced_value)
+        listView = root.findViewById<ListView>(R.id.home_recent_informations)
 
         val gd = GradientDrawable(
             GradientDrawable.Orientation.BL_TR, intArrayOf(Color.parseColor(PrivateSettings.gradientStart), Color.parseColor(PrivateSettings.gradientStop)))
         gd.cornerRadius = 50f
         sync_btn.background = gd
 
-        // Recent Information ScrollView
-        // Test Element
-/*        titles.add("Title 1")
-        titles.add("Title 2")
-        titles.add("Title 3")
-        titles.add("Title 4")
-        descriptions.add("Description 1")
-        descriptions.add("Description 2")
-        descriptions.add("Description 3")
-        descriptions.add("Description 4")
-        createdAts.add("Created At 1")
-        createdAts.add("Created At 2")
-        createdAts.add("Created At 3")
-        createdAts.add("Created At 4")*/
-
         sync_btn.setOnClickListener {
             val scanDao: ScanDao = dbvar.scanDao()
-            var unsentData: List<Scan>
+            var unsyncedData: List<Scan>
 
             CoroutineScope(Dispatchers.Main).launch {
-                unsentData = scanDao.getUnsynced()
-                for (element in unsentData) {
+                unsyncedData = scanDao.getUnsynced()
+                for (element in unsyncedData) {
                     sendToOnlineDatabase(element.insectId,element.latitude,element.longitude,element.altitude,element.timestamp)
                     scanDao.setSynced(element.scan_id, 1)
                 }
                 updateDataShown()
             }
         }
-
-        // Reverse that latest is up
-/*        titles.reverse()
-        descriptions.reverse()
-        createdAts.reverse()
-
-        // Load ScrollView
-        val infoAdapter = HomeNewsListAdapter((activity as MainActivity), titles.toTypedArray(), descriptions.toTypedArray(), createdAts.toTypedArray())
-        val listView = root.findViewById<ListView>(R.id.home_recent_informations)
-        listView.adapter = infoAdapter*/
 
         return root
     }
@@ -100,8 +79,26 @@ class HomeFragment : Fragment() {
 
     fun updateDataShown() {
         val scanDao: ScanDao = dbvar.scanDao()
+        var unsyncedData: List<Scan>
         CoroutineScope(Dispatchers.Main).launch {
-            scanView.text = scanDao.getUnsynced().toString()
+            titles.clear()
+            descriptions.clear()
+            createdAts.clear()
+            unsyncedData = scanDao.getUnsynced()
+            val scanStr: String = getString(R.string.scan)
+            val idStr: String = getString(R.string.id)
+            for (element in unsyncedData) {
+                sendToOnlineDatabase(element.insectId,element.latitude,element.longitude,element.altitude,element.timestamp)
+                titles.add(scanStr + " " + element.scan_id)
+                descriptions.add(idStr + " " + element.insectId)
+                createdAts.add(element.timestamp)
+             }
+            // Reverse that latest is up
+            titles.reverse()
+            descriptions.reverse()
+            createdAts.reverse()
+            var infoAdapter = HomeNewsListAdapter((activity as MainActivity), titles.toTypedArray(), descriptions.toTypedArray(), createdAts.toTypedArray())
+            listView.adapter = infoAdapter
             totalScannedView.text = scanDao.getNumberOfColumns().toString()
             numberTypesView.text = scanDao.getNumberOfTypes().toString()
             numberUnsyncedView.text = scanDao.getNumberOfUnsynced().toString()
